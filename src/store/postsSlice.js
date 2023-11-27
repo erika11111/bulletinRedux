89d8d6b6482e505177759f37064016b647c5d143
erika11111 <104774933+erika11111@users.noreply.github.com>
2023-11-27 19:26:06 +0100

    First commit

---
+++ src/store/postsSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+  },
+];
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+});
+export default postsSlice.reducer;
